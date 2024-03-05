f6666ab6783ffc00e3232fd61830097f6d8a3211
Amrinder Singh <amrinder995@gmail.com>
2024-03-04 21:51:07 -0500

    added tail function

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual =  function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArr = [];
+  for (let i = 1; i < array.length; i++) {
+    newArr.push(array[i]);
+  }
+  return newArr;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
