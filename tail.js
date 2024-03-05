ee79a6dc80b74622ed945838a4bc1d3acf66e5d0
Amrinder Singh <amrinder995@gmail.com>
2024-03-04 21:57:17 -0500

    added tests for empty array and one element arrays

--- tail.js
+++ tail.js
@@ -17,3 +17,13 @@ const tail = function(array) {
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); 
+
+const empty = [];
+console.log(tail(empty));
+
+const oneword = ["Hello"];
+console.log(tail(oneword));
