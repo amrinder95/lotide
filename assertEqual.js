a1dd313bd421a990706d6a42f7bac0a420960d74
Amrinder Singh <amrinder995@gmail.com>
2024-03-04 20:56:53 -0500

    added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual =  function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Hello", "Hello");
+assertEqual(1, 1);
+assertEqual(1, 2);
