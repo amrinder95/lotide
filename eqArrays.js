cf6da3528c47bcf5553a6856274b5ceb6294890d
Amrinder Singh <amrinder995@gmail.com>
2024-03-05 19:55:02 -0500

    refactored code

--- eqArrays.js
+++ eqArrays.js
@@ -10,18 +10,14 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false
   }
-  let falses = 0;
   for(let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      falses ++;
-    }
-  }
-  if (falses > 0) {
-    return false
-  } else {
+      return false
+    } 
+  } 
     return true
-  }
-}
+};
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
