616970ee562b22355a6d5b926b7504c535d05228
Amrinder Singh <amrinder995@gmail.com>
2024-03-06 19:47:54 -0500

    created letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for(let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    } 
+  } 
+    return true
+};
+
+const assertArraysEqual =  function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion passed: ${arrayOne} === ${arrayTwo}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`)
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for(let letter of sentence) {
+    results[letter] = [];
+  }
+  for(let i = 0; i < sentence.length; i++) {
+   
+  }
+  console.log(results);
+  return results;
+};
+
+letterPositions("hello");
