90f76174baa609850f179fd0d86bec9c588ba44d
Amrinder Singh <amrinder995@gmail.com>
2024-03-07 21:19:43 -0500

    added test cases

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for(let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    } 
+  } 
+    return true
+};
+
+const assertArraysEqual =  function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion passed: ${arrayOne} === ${arrayTwo}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`)
+  }
+};
+
 const takeUntil = function(array, callback) {
   const results = [];
   for (let element of array) {
@@ -22,3 +42,13 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+console.log('---')
+
+const data3 = ["Empty", "1/4", "1/2", "3/4", "Full", "Overflow", "Overflow", "Overflow"]
+const results3 = takeUntil(data3, x => x === "Overflow")
+console.log(results3)
+
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+assertArraysEqual(results3, ["Empty", "1/4", "1/2", "3/4", "Full"]);
