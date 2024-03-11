4d36c7eb174f1a6c1503ca9283d9f3306b21f813
Amrinder Singh <amrinder995@gmail.com>
2024-03-11 15:23:48 -0400

    refactored and added tailTest file

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual =  function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 const tail = function(array) {
   let newArr = [];
   for (let i = 1; i < array.length; i++) {
@@ -13,16 +6,5 @@ const tail = function(array) {
   }
   return newArr;
 };
-const words = ["Hello", "Lighthouse", "Labs"];
-const originalLength = words.length;
-const result = tail(words);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
-assertEqual(originalLength, words.length);
-
-const empty = [];
-assertEqual(tail(empty).length, 0);
-
-const oneword = ["Hello"];
-assertEqual(tail(oneword).length, 0);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,16 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+const words = ["Hello", "Lighthouse", "Labs"];
+const originalLength = words.length;
+const result = tail(words);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+assertEqual(originalLength, words.length);
+
+const empty = [];
+assertEqual(tail(empty).length, 0);
+
+const oneword = ["Hello"];
+assertEqual(tail(oneword).length, 0);
