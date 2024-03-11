9b9fc77859ec7c3db952cb832cd507bfe6134206
Amrinder Singh <amrinder995@gmail.com>
2024-03-11 15:41:43 -0400

    refactored

--- middle.js
+++ middle.js
@@ -5,7 +5,7 @@ const middle = function(array) {
       let evenIndex2 = array.length / 2
       let evenIndex1 = evenIndex2 - 1
       newArray.push(array[evenIndex1], array[evenIndex2]) 
-    } if (array.length % 2 === 1) {
+    } else {
       let oddIndex = Math.floor(array.length / 2)
       newArray.push(array[oddIndex])
     }
