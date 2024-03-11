98583b89b9b6322ab27832aebbf3f50ddd43af80
Amrinder Singh <amrinder995@gmail.com>
2024-03-11 18:07:26 -0400

    refactored and added middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual((middle([1])), []);
-assertArraysEqual((middle([1, 2])), []);
-assertArraysEqual((middle([1, 2, 3])), [2]);
-assertArraysEqual((middle([1, 2, 3, 4 ,5])), [3]);
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]);
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);
-
+describe('#middle', () => {
+  it("Returns an empty array when passed 1 element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("Returns an empty array when passed 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("Returns the middle element when passed an odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("Returns the middle two elements when passed an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
